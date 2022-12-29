35aa44271214d1b6ab67822cae225ae8cee780fc
Deepjyoti Roy <89153853+djroy366@users.noreply.github.com>
2022-12-29 21:24:37 +0530

    Update main.c
    Basic layout of the program

--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include "shell.h"
+#include "shell.h" // need to make this header file for function prototypes
 
 int main(int argc, char **argv)
-{
-    
+{   int status;
+       
+    while(status==1)
+    {   int i;
+        printmgs();
+        i=read();
+        do_something();
+        status=i;
+    }    
 }

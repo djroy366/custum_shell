d04578a5a1647bb6a12c494716c5feb96f8e5728
Deepjyoti <Djroy366@gmail.com>
2024-01-22 13:01:12 +0530

    changes: moved the loop from main function to a function defined the functio prototypes, will compile and check

--- main.c
+++ main.c
@@ -4,14 +4,41 @@
 #include <string.h>
 #include "shell.h" // need to make this header file for function prototypes
 
+
+int status; // Asof now , a global variable given
+
+
+void printmgs(char * input) // As of now this will print the input
+{
+	printf("%s",input);
+}
+
+void read(void);
+
+void do_something(void);
+
+
+
+void loop(void)
+{
+	do{                //moved this from the main function
+		int i;
+		printmgs();
+		i=read();
+		do_something();
+		status=i;
+
+	}
+
+	while(status==1)
+
+}
+
+
+
 int main(int argc, char **argv)
-{   int status;
-       
-    while(status==1)
-    {   int i;
-        printmgs();
-        i=read();
-        do_something();
-        status=i;
-    }    
+{
+
+	loop();	
+
 }
---
+++ main.h
@@ -0,0 +1,1 @@
+// Will add everything here
